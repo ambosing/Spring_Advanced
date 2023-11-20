95cce3092db930be4bc196361aea6f3ecc46c2cc
ambosing <ambosing_@naver.com>
2023-11-20 18:26:17 +0900

    데코레이터 패턴과 프록시 패턴

---
+++ proxy/src/main/java/hello/proxy/app/v2/OrderRepositoryV2.java
@@ -0,0 +1,19 @@
+package hello.proxy.app.v2;
+
+public class OrderRepositoryV2 {
+    public void save(String itemId) {
+        //저장 로직이라고 가정
+        if (itemId.equals("ex")) {
+            throw new IllegalStateException("예외 발생!");
+        }
+        sleep(1000);
+    }
+
+    private static void sleep(int millis) {
+        try {
+            Thread.sleep(millis);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
+}
